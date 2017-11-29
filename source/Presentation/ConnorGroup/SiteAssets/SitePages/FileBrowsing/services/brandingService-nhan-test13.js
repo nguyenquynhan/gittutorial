b2fd83b2d67cb8851ba0382136c13d024b1cce8d
Nhan Nguyen <nhan.nguyen@soldenservices.com>
2017-11-29 17:36:34 +0700

    FileBrowser - Add new widget by Automation

---
+++ source/Presentation/ConnorGroup/SiteAssets/SitePages/FileBrowsing/services/brandingService-nhan-test13.js
@@ -0,0 +1,10 @@
+function brandingService() {
+    return {
+        //rootId: 'THftjXxjg0a', 
+        rootId: 'vTYSeNQpXki', //dev
+        baseUrl: 'https://insiderfilesdev.soldenservices.com',
+        homePath: '/',
+        nativeFileSharePath: '\\\\10.10.1.23\\TestShared',
+        nativeFileSharePathMac: '\\\\10.10.1.23\\TestSharedMac'
+    }
+};
