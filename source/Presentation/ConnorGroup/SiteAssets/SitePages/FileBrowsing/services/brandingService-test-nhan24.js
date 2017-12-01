7860c51bc490424606421e2ea457f084972bd6cb
Nhan Nguyen <nhan.nguyen@soldenservices.com>
2017-12-01 10:11:19 +0700

    FileBrowser - Add new widget for share path '\\10.10.1.23\TestShared' by Automation

---
+++ source/Presentation/ConnorGroup/SiteAssets/SitePages/FileBrowsing/services/brandingService-test-nhan24.js
@@ -0,0 +1,10 @@
+function brandingService() {
+    return {
+        //rootId: 'THftjXxjg0a', 
+        rootId: 'vTYSeNQpXki', //dev
+        baseUrl: 'https://insiderfilesdev.soldenservices.com',
+        homePath: '/',
+        nativeFileSharePath: '\\\\10.10.1.23\\TestShared',
+        nativeFileSharePathMac: 'smb://10.10.1.23/TestShared'
+    }
+};
